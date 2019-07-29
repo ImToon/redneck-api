d21a95db88944a123fec76a8e54f4b59c24c9db3
Guillaume <gservais98@gmail.com>
2019-07-29 14:57:58 +0200

    Send json as response

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const controller = "user";
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -9,15 +8,15 @@ router.post(`/register`, async (req,res)=>{
 
     const {error} = registerValidation(req.body);
     if(error)
-        return res.status(400).send(error.details[0]);
+        return res.status(400).send({error : error.details[0]});
 
     const emailExist = await User.findOne({email:req.body.email});
     if(emailExist)
-        return res.status(400).send('Email already used');
+        return res.status(400).send({error : 'Email already used'});
 
     const usernameExist = await User.findOne({username:req.body.username});
     if(usernameExist)
-        return res.status(400).send('Email already used');
+        return res.status(400).send({error : 'Email already used'});
 
     const salt = await bcrypt.genSaltSync(10);
     const hash = await bcrypt.hashSync(req.body.password, salt);
@@ -30,19 +29,19 @@ router.post(`/register`, async (req,res)=>{
     try {
         const savedUser = await newUser.save();
         res.send(savedUser);
-    } catch (error) {
-        res.status(400).send(error);
+    } catch (err) {
+        res.status(400).send({error : err});
     }
 });
 
 router.post(`/login`, async (req,res)=>{
     const {error} = loginValidation(req.body);
     if(error)
-        return res.status(400).send(error.details[0]);
+        return res.status(400).send({error: error.details[0]});
 
     const userFound = await User.findOne({username:req.body.username});
     if(!userFound)
-        return res.status(404).send('Username not found');
+        return res.status(404).send({error:'Username not found'});
     
     const isPasswordCorrect = await bcrypt.compare(req.body.password, userFound.password);
     if(isPasswordCorrect){
@@ -50,7 +49,7 @@ router.post(`/login`, async (req,res)=>{
         res.send({token : token});
     }   
     else
-        return res.status(401).send('Username or password is wrong');
+        return res.status(401).send({error : 'Username or password is wrong'});
 });
 
 module.exports = router;
